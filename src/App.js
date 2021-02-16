ecb84fb03fbd4b27f558b18f36d51796f78e3e0b
mocuishla <misabellanes@gmail.com>
2021-02-16 13:32:01 +0100

    Improve design

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://logodix.com/logo/2134241.png" alt="logo" />
+        <form>
+  <label>
+    Pega aquí:
+    <br></br>
+    <input type="textarea" className="" rows="4" cols="50" name="pega aquí" />
+  </label>
+  <input type="submit" value="ok" />
+</form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ---
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Hello World
-        </a>
+        
       </header>
     </div>
   );
