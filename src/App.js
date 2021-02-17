1bdc5695df948ab461cdf65fb51c8b0a0d96ff34
mocuishla <misabellanes@gmail.com>
2021-02-17 23:57:47 +0100

    first working version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import transform from "./transformation";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [originalText, setOriginalText] = useState("");
+  const [outputText, setOutputText] = useState("");
+
+  function processText() {
+    const transformText = transform(originalText)
+    setOutputText(transformText);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://logodix.com/logo/2134241.png" alt="logo" />
         <form>
-  <label>
-    Pega aquí:
-    <br></br>
-    <input type="textarea" className="" rows="4" cols="50" name="pega aquí" />
-  </label>
-  <input type="submit" value="ok" />
-</form>
-        <p>
-          ---
-        </p>
-        
+          <TextField
+            label="Pega aquí"
+            multiline
+            rows={10}
+            color="primary"
+            variant="outlined"
+            value={originalText}
+            onChange={event => setOriginalText(event.target.value)}
+          />
+        </form>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={event => processText()}
+        >
+          Click
+        </Button>
+
+        <form>
+          <TextField
+            label="Pega aquí"
+            multiline
+            rows={10}
+            color="primary"
+            variant="outlined"
+            value={outputText}
+          />
+        </form>
       </header>
     </div>
   );
